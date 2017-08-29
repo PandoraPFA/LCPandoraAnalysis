7459c321d552fbb1700ea08de4992e743839cafc
Ete Remi <remi.ete@desy.de>
2017-08-29 14:42:12 +0200

    Removed unneeded branches and variables in event selection

--- calibration/PandoraPFACalibrate_SoftwareCompensation.cc
+++ calibration/PandoraPFACalibrate_SoftwareCompensation.cc
@@ -216,9 +216,7 @@ private:
     
 private:
     TChain         *m_pTChain;                ///< Chain of root files
-    int             m_nPfos;                  ///< Number of pfos
-    int             m_nClusters;              ///< Number of clusters
-    FloatVector    *m_pfoEnergies;            ///< Pfo energies
+    float           m_pfoEnergy;              ///< Pfo energy
     FloatVector    *m_hitEnergies;            ///< All hit energies (ECal + HCal + Muon)
     FloatVector    *m_cellSize0;              ///< Cell sizes 0
     FloatVector    *m_cellSize1;              ///< Cell sizes 1
@@ -317,7 +315,7 @@ int main(int argc, char **argv)
 
 Event::Event(const SoftwareCompensation &softwareCompensation) :
     m_trueEnergy(softwareCompensation.m_trueEnergy),
-    m_reconstructedEnergy(softwareCompensation.m_pfoEnergies->at(0)),
+    m_reconstructedEnergy(softwareCompensation.m_pfoEnergy),
     m_eCalEnergy(0.f),
     m_hCalBinEnergies(),
     m_hCalHitEnergies()
@@ -371,9 +369,7 @@ SoftwareCompensation::SoftwareCompensation() :
     m_softCompFinalParameters(),
     m_eventVector(),
     m_pTChain(NULL),
-    m_nPfos(0),
-    m_nClusters(0),
-    m_pfoEnergies(NULL),
+    m_pfoEnergy(0.f),
     m_hitEnergies(NULL),
     m_cellSize0(NULL),
     m_cellSize1(NULL),
@@ -435,7 +431,7 @@ bool SoftwareCompensation::SkipCurrentEvent() const
     if(m_hitEnergies->empty())
         return true;
         
-    const bool invalidSingleParticle(m_nPfos != 1 || m_nClusters != 1 || m_pfoEnergies->at(0) <= 0);
+    const bool invalidSingleParticle(m_pfoEnergy <= 0);
     
     if(invalidSingleParticle)
         return true;
@@ -751,9 +747,7 @@ TCanvas *SoftwareCompensation::DrawPlots(const std::vector<T*> &plots, TLegend *
 
 void SoftwareCompensation::SetBranchAddresses()
 {
-    m_pTChain->SetBranchAddress("numberOfPfos",&m_nPfos);
-    m_pTChain->SetBranchAddress("numberOfClusters",&m_nClusters);
-    m_pTChain->SetBranchAddress("EnergyOfPfos",&m_pfoEnergies);
+    m_pTChain->SetBranchAddress("EnergyOfPfo",&m_pfoEnergy);
     m_pTChain->SetBranchAddress("HitEnergies",&m_hitEnergies);
     m_pTChain->SetBranchAddress("CellSize0",&m_cellSize0);
     m_pTChain->SetBranchAddress("CellSize1", &m_cellSize1);
